aa9488e03a1c4432461b8bf1d1be7a7b5bbe28d9
VinRanggaSr <vrangga1@gmail.com>
2023-11-01 14:20:00 +0700

    responses

--- router/users.js
+++ router/users.js
@@ -6,12 +6,30 @@ let users = [  //buat data dummy
     {id: 2, nama: "Rangga", email: "rangga@gmail.com"},
 ]
 router.get('/users', (req, res) => {
-    res.json(users)
+    if(users.length > 0){ //kondisi jika ada data
+        res.json({
+            status: true,
+            data: users,
+            method: req.method,
+            url: req.url
+        })
+    }else{
+        res.json({
+            status: false,
+            message: "data masih kosong"
+        })
+    }
   })
 
   router.post('/user', (req, res) => { //menyimpan atau menambahkan
     users.push(req.body) //menyimpan apa yg ada di body kedalam users
-    res.json(users)
+    res.json({
+        status: true,
+        data: users,
+        method: req.method,
+        url: req.url,
+        message: "data berhasil ditambah"
+    })
   })
 
   router.put('/user/:id', (req, res) => { // nyimpan perubahan data dgn id tertentu
@@ -23,14 +41,26 @@ router.get('/users', (req, res) => {
             return user
         }
     })
-    res.json(users)
+    res.json({
+        status: true,
+        data: users,
+        method: req.method,
+        url: req.url,
+        message: "data berhasil dirubah"
+    })
   })
 
   router.delete('/user/:id', (req, res) => { //hapus id user dgn id tertentu
     const id = req.params.id
     users = users.filter(user => user.id != id)
 
-    res.json(users)
+    res.json({
+        status: true,
+        data: users,
+        method: req.method,
+        url: req.url,
+        message: "data berhasil dihapus"
+    })
   })
 
   module.exports = router
