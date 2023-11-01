e466f3585012b9f90fc5ebc1c7911c1b0b9fc533
VinRanggaSr <vrangga1@gmail.com>
2023-11-01 11:48:05 +0700

    Basic-routing&method-HTTP

--- index.js
+++ index.js
@@ -6,6 +6,22 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/users', (req, res) => {
+    res.send('Got All Users') //menampilkan
+  })
+
+  app.post('/user', (req, res) => {
+    res.send('Got a POST request') //menyimpan atau menambahkan
+  })
+
+  app.put('/user/:id', (req, res) => {
+    res.send('Got a PUT request at /user') // nyimpan perubahan data dgn id tertentu
+  })
+
+  app.delete('/user/:id', (req, res) => {
+    res.send('Got a DELETE request at /user') //hapus id user dgn id tertentu
+  })
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
